57f27e0a0a051ba0bfa5241d336de40284e5f2e0
ovi <ovi.daniel.b@gmail.com>
2017-03-22 01:03:07 +0100

    resolved simplified first requirement; extended model classes

--- src/main/java/org/uniroma2/sdcc/Model/Address.java
+++ src/main/java/org/uniroma2/sdcc/Model/Address.java
@@ -1,25 +1,35 @@
 package org.uniroma2.sdcc.Model;
 
-import java.io.Serializable;
+/**
+ * Created by ovidiudanielbarba on 16/03/2017.
+ */
 
 /**
  * defines a city address
  */
-public class Address implements Serializable {
-
-    private static final long serialVersionUID = 1L;
+public class Address {
 
     private String name;
+    private AddressType addressType;
     private int number;
     private AddressNumberType numberType;
 
     public Address() {
     }
 
-    public Address(String name, int number, AddressNumberType numberType) {
+    public Address(AddressType addressType, String name, int number, AddressNumberType numberType) {
+        this.addressType = addressType;
         this.name = name;
         this.number = number;
         this.numberType = numberType;
+    }
+
+    public AddressType getAddressType() {
+        return addressType;
+    }
+
+    public void setAddressType(AddressType addressType) {
+        this.addressType = addressType;
     }
 
     public String getName() {
@@ -46,7 +56,13 @@ public class Address implements Serializable {
         this.numberType = numberType;
     }
 
+    @Override
     public String toString() {
-        return name + " " + numberType.toString().toLowerCase() + " " + number;
+        return "Address{" +
+                "name='" + name + '\'' +
+                ", addressType=" + addressType +
+                ", number=" + number +
+                ", numberType=" + numberType +
+                '}';
     }
 }
